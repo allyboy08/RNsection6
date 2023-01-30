914739ff83417cf1a7625815699305082a9cc8b4
Alex <alexs@makebetter.co.za>
2023-01-30 16:07:32 +0200

    new

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,40 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+
+// import { useContext } from 'react';
+import MealsList from '../components/MealsList/MealsList';
+import { MEALS } from '../data/dummy-data';
+// import { FavoritesContext } from '../store/context/favorites-context';
+
+
+function FavoritesScreen() {
+    // const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMealIds = useSelector(state => state.favoriteMeals.ids)
+
+    const favoriteMeals = MEALS.filter((meal) => favoriteMealIds.includes(meal.id));
+
+    if (favoriteMeals.length === 0) {
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>you have no favorite meals</Text>
+            </View>
+        )
+    }
+
+    return <MealsList items={favoriteMeals} />
+}
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
